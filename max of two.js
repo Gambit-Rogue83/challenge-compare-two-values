c86c4c805c588c295573c5b90a11c47f00c3db43
Shattuck Kuzmic <shattuckkuzmic@gmail.com>
2023-12-21 14:26:35 -0800

    challenge 1

---
+++ max of two.js
@@ -0,0 +1,18 @@
+//This code determines which variable is greater in value
+
+// let x = 1
+// let y = 2
+
+// let x = 4
+// let y = 2
+
+let x = 3
+let y = 3
+
+if(x > y) {
+    console.log(x)
+} else if(x < y) {
+    console.log(y)
+} else {
+    console.log('The values are identical')
+}
